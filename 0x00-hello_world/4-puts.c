8e1d4184e0482ae3594af9f4b66a273297842566
Nabil Mustapha <nmus.nm@gmail.com>
2022-07-03 14:41:04 -0700

    Committing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* 
- * Betty documentation needed 
-*/
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
