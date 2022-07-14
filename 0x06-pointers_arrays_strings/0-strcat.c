52b47ce2aaa6fb7a5131869ca9b14786a83fb353
Nabil Mustapha <nmus.nm@gmail.com>
2022-07-14 13:32:39 -0700

    Committing

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,8 @@
  *  * _strcat - concatenate two strings
  *   * @dest: char string to concatenate to
  *    * @src: char string
- *     * Return: pointer to resulting string `dest`
-       */
+ *     * Return: pointer to resulting string `dest`      
+ */
 
 char *_strcat(char *dest, char *src)
 {
